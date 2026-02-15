6501e1447a65316b83ac8e2b04cb523fb2615f1f
AutoCAR Agent <autocar-agent@autopath.dev>
2026-02-15 14:11:27 +0000

    AutoCAR Agent: FAS-1 - Test

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -7,7 +7,7 @@ const HomePage = () => {
       <Navbar />
       <section className="flex flex-col items-center justify-center min-h-screen bg-red-500">
         <div className="text-center p-10">
-          <h1 className="text-5xl font-bold text-gray-800 mb-4">Welcome to the Chirpn</h1>
+          <h1 className="text-5xl font-bold text-white mb-4">Welcome to the Chirpn</h1>
         </div>
       </section>
     </>
