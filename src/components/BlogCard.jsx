8e58310a58e770f1d80902e22f27047f2cd1710a
AutoCAR Agent <autocar-agent@autopath.dev>
2026-02-18 07:35:29 +0000

    AutoCAR Agent: FAS-7 - Setup Project Folder Structure for Fashion Blog

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -5,7 +5,7 @@ const BlogCard = ({ blog }) => {
   return (
     <article className="bg-white rounded-lg shadow-md overflow-hidden">
       <img
-        src={blog.image}
+        src="https://th.bing.com/th/id/OIP.qy8gLIbvZO-8ox82MVM9tAHaEK?w=274&h=180&c=7&r=0&o=7&cb=defcache2&dpr=1.3&pid=1.7&rm=3&defcache=1"
         alt={blog.title}
         className="w-full h-48 object-cover"
       />
